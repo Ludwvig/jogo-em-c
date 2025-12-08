9697f5799abf5042b728007741220ab9d316caa1
Felipe <felpswes@gmail.com>
2025-12-08 12:29:37 -0300

    Reformulando comentários

--- jogo.c
+++ jogo.c
@@ -3,28 +3,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// Definição fundamental 
+/*Ponteiro: é uma variável que guarda o endereço de memória de alguma coisa.
+Logo quando criado, aponta para um endereço aleatório e não utilizável (lixo de memória).
+É necessário apontar para um endereço de memória específico se quiser utilizá-lo devidamente.*/
+// checklist:
+/* 1- Incluir as seguintes bibliotecas: 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>*/
+// 2- Cria uma struct 
+// 3- Criar dois ponteiros auxiliares: inicio e anterior, que esperam o endereço de um Item
+
+
 typedef struct Item {
     char nome[40];
     char tipo[10]; 
     int quantidade;
     struct Item *proximo; 
 } Item;
-// ponteiro: uma variável que pode guardar o endereço do qual se refere (do lado esquerdo do *) 
+
+
 
 
 Item *inicio = NULL;
 Item *anterior = NULL;
 
-// checklist: 
-// 1- criar função que retorne um ponteiro para um Item 
-// 2- cria um ponteiro novo que pode apontar pra um Item
-// 3- verifica se tem memória 
-// 4- copia os caracteres de cada argumento e garante que o usuário só vai preencher até o penúltimo
-// 5- acessa o último caractere e coloca um '\0', fim da leitura 
-// 6- 
-
-Item *criarItem(const char *nome, const char *tipo) { // adicionar parâmetros e tipos de cada um, utilizando ponteiros
-    Item *novo = malloc(sizeof(Item)); // criar espaço na memória para um ponteiro * novo do tipo Item
+Item *criarItem(const char *nome, const char *tipo) { 
+    Item *novo = malloc(sizeof(Item)); // 
     if (!novo) return NULL; // checar se malloc conseguiu dar um retorno válido de memória para novo
     strncpy(novo->nome, nome, sizeof(novo->nome)-1); // acessa o campo nome, copia o argumento, força o tamanho do campo a ter 39 (com 0, 40) chars
     novo->nome[sizeof(novo->nome)-1] = '\0'; // acessa o último char do campo nome e coloca quebra de array
